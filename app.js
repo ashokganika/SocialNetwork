4a0bc43a51a506504ad964b2672c85ae02c84d27
Ashok <ashok.ag.ganika@gmail.com>
2020-11-07 19:43:58 +0545

    cleaned code

--- app.js
+++ app.js
@@ -1,51 +1,44 @@
-var express = require('express');
-var createError = require('http-errors');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-var apiRouter = require('./routes/api.routes');
-var cors = require('cors');
-
+var express = require("express");
+var path = require("path");
+var logger = require("morgan");
+var apiRouter = require("./routes/api.routes");
+var cors = require("cors");
 
 const port = process.env.PORT || 8001;
 
 var app = express();
-require('./socket');
-require('./db');
+require("./socket");
+require("./db");
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
+app.use(express.static("uplaods/images"));
+app.use("/file", express.static(path.join(__dirname, "uploads/images")));
 
-app.use(express.static('uplaods/images'));
-app.use('/file', express.static(path.join(__dirname,'uploads/images')));
-
-app.use('/api', apiRouter);
-
+app.use("/api", apiRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404))
+app.use(function (req, res, next) {
+  next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   console.log("i am at error handling middleware...", err);
-  res.status(err.status || 400)
+  res.status(err.status || 400);
   // console.log(err);
   res.json(err);
 });
 
-
 //server listening port
-app.listen(port, function(err, done){
-  if(err){
+app.listen(port, function (err, done) {
+  if (err) {
     console.log("connection failed", err);
-  }
-  else{
+  } else {
     console.log("listening at port ", port);
   }
-})
+});
